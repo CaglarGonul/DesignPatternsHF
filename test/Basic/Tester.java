22d22700e08672c446d8891ee1b6ed2fbc9b5e40
caglar <caglar@ubuntu>
2013-04-06 11:52:53 +0300

    Test cases expanded.

--- test/Basic/Tester.java
+++ test/Basic/Tester.java
@@ -1,15 +1,31 @@
 package Basic;
 
-import com.chp1.simuduck.v1.RubberDuck;
+
+
+import com.chp1.simuduck.v3.Animal;
+import com.chp1.simuduck.v3.Cat;
+import com.chp1.simuduck.v3.Dog;
 import org.junit.Test;
 
 public class Tester {
-    
-    @Test
+
     public void test_simuduck() {
-        RubberDuck rd = new RubberDuck();
-        rd.quack();
-        rd.display();
-        rd.fly();
+        com.chp1.simuduck.v1.RubberDuck rd1 = new com.chp1.simuduck.v1.RubberDuck();
+        rd1.quack();
+        rd1.display();
+        rd1.fly();
+        
+        com.chp1.simuduck.v2.RubberDuck rd2 = new com.chp1.simuduck.v2.RubberDuck();
+        rd2.quack();
+        rd2.display();
     }
+    
+    @Test 
+    public void test_animal(){
+        Animal animal = new Dog();
+        animal.makeSound();
+        animal = new Cat();
+        animal.makeSound();
+    }
+    
 }
