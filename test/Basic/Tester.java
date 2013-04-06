731f64c1776bde606293fe489cfedde93c4b7e3a
caglar <caglar@ubuntu>
2013-04-06 14:46:07 +0300

    Simuduck v3 tester is implemented.

--- test/Basic/Tester.java
+++ test/Basic/Tester.java
@@ -2,14 +2,16 @@ package Basic;
 
 
 
-import com.chp1.simuduck.v3.Animal;
-import com.chp1.simuduck.v3.Cat;
-import com.chp1.simuduck.v3.Dog;
+import com.chp1.program.to.supertype.Animal;
+import com.chp1.program.to.supertype.Cat;
+import com.chp1.program.to.supertype.Dog;
+import com.chp1.simuduck.v3.FlyNoWay;
+import com.chp1.simuduck.v3.Squeak;
 import org.junit.Test;
 
 public class Tester {
 
-    public void test_simuduck() {
+    public void test_simuduck_v1_v2() {
         com.chp1.simuduck.v1.RubberDuck rd1 = new com.chp1.simuduck.v1.RubberDuck();
         rd1.quack();
         rd1.display();
@@ -20,12 +22,27 @@ public class Tester {
         rd2.display();
     }
     
-    @Test 
     public void test_animal(){
         Animal animal = new Dog();
         animal.makeSound();
         animal = new Cat();
         animal.makeSound();
+    }
+    
+    @Test
+    public void test_simuduck_v3(){
+        com.chp1.simuduck.v3.MallardDuck md = new com.chp1.simuduck.v3.MallardDuck();
+        md.performQuack();
+        md.performFly();
+        
+        //Lets change quack behaviour at run time
+        md.setQuackBehaviour(new Squeak());
+        md.performQuack();
+        
+        //Lets change fly behaviour at run time
+        md.setFlyBehaviour(new FlyNoWay());
+        md.performFly();
+        
     }
     
 }
